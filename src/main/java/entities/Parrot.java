4fae6f292716da57df4a28e61fb02e8f068707ec
Admin <lemsviat@gmail.com>
2018-09-18 09:45:00 +0300

    Commit 1

---
+++ src/main/java/entities/Parrot.java
@@ -0,0 +1,16 @@
+package entities;
+
+import org.springframework.stereotype.Component;
+
+@Component ("parrot-kesha")
+public class Parrot {
+    private String name= "Кеша";
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
